619c4624c18a89f9a096e838a00a8f36cdb7cbc4
Aleksadra Sakharova <Al.kikka@yandex.ru>
2025-07-06 17:21:15 +0300

    Удалено дублирование создания курьера в CourierTests

--- src/test/java/org/example/CourierTests.java
+++ src/test/java/org/example/CourierTests.java
@@ -5,10 +5,8 @@ import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.Response;
 import org.example.api.CourierApi;
 import org.example.models.CourierModel;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-
+import com.github.javafaker.Faker;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static io.restassured.RestAssured.given;
@@ -16,33 +14,13 @@ import static org.hamcrest.Matchers.*;
 
 public class CourierTests {
     private static final ObjectMapper mapper = new ObjectMapper();
-    private String courierId;
-    private String login;
-
-    @Before
-    public void setUp() throws Exception {
-        login = "test" + System.currentTimeMillis(); // Уникальный логин
-        CourierModel courier = new CourierModel(login, "1234", null);
-        Response response = CourierApi.createCourier(courier);
-        response.then()
-                .log().all()
-                .statusCode(201);
-        courierId = response.jsonPath().getString("id"); // Проверяем, есть ли id
-        System.out.println("Created courierId: " + courierId);
-    }
-
-    @After
-    public void cleanUp() {
-        if (courierId != null) {
-            CourierApi.deleteCourier(courierId);
-        }
-    }
+    private static final Faker faker = new Faker();
 
     @Test
     @DisplayName("Test creating courier")
     @Description("Test creating courier with valid data")
     public void testCreateCourier() throws Exception {
-        String testLogin = "test" + System.currentTimeMillis();
+        String testLogin = "test_" + faker.name().username();
         CourierModel courier = new CourierModel(testLogin, "1234", null);
         Response response = CourierApi.createCourier(courier);
         response.then()
@@ -52,10 +30,23 @@ public class CourierTests {
     }
 
     @Test
+    @DisplayName("Test creating courier without password")
+    @Description("Test that creating courier without password fails")
+    public void testCreateCourierWithoutPassword() throws Exception {
+        String testLogin = "test_" + faker.name().username();
+        CourierModel courier = new CourierModel(testLogin, "", null);
+        Response response = CourierApi.createCourier(courier);
+        response.then()
+                .log().all()
+                .statusCode(400)
+                .body("message", equalTo("Недостаточно данных для создания учетной записи"));
+    }
+
+    @Test
     @DisplayName("Test creating courier without login")
     @Description("Test that creating courier without login fails")
     public void testCreateCourierWithoutLogin() throws Exception {
-        CourierModel courier = new CourierModel("", "1234", null); // Без логина и firstName
+        CourierModel courier = new CourierModel("", "1234", null);
         Response response = CourierApi.createCourier(courier);
         response.then()
                 .log().all()
