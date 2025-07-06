265e57fa8c71234e3469c17cd99c5e5c5f956b22
Aleksadra Sakharova <Al.kikka@yandex.ru>
2025-07-06 12:49:30 +0300

    Добавление теста на создание курьера без логина и без firstName

--- src/test/java/org/example/CourierTests.java
+++ src/test/java/org/example/CourierTests.java
@@ -2,94 +2,63 @@ package org.example;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.junit4.DisplayName;
-import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.example.api.CourierApi;
+import org.example.models.CourierModel;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 public class CourierTests {
-    private static final String BASE_URL = "https://qa-scooter.praktikum-services.ru";
+    private static final ObjectMapper mapper = new ObjectMapper();
     private String courierId;
+    private String login;
 
     @Before
-    public void setUp() {
-        RestAssured.baseURI = BASE_URL;
+    public void setUp() throws Exception {
+        login = "test" + System.currentTimeMillis(); // Уникальный логин
+        CourierModel courier = new CourierModel(login, "1234", null);
+        Response response = CourierApi.createCourier(courier);
+        response.then()
+                .log().all()
+                .statusCode(201);
+        courierId = response.jsonPath().getString("id"); // Проверяем, есть ли id
+        System.out.println("Created courierId: " + courierId);
     }
 
     @After
-    public void tearDown() {
+    public void cleanUp() {
         if (courierId != null) {
-            given()
-                    .header("Content-Type", "application/json")
-                    .delete("/api/v1/courier/" + courierId);
+            CourierApi.deleteCourier(courierId);
         }
     }
 
     @Test
-    @DisplayName("Create courier successfully")
-    @Description("Test that a courier can be created with valid data")
-    public void testCreateCourier() {
-        String login = "ninja" + System.currentTimeMillis();
-        String body = "{\"login\": \"" + login + "\", \"password\": \"1234\", \"firstName\": \"saske\"}";
-        Response response = given()
-                .header("Content-Type", "application/json")
-                .body(body)
-                .post("/api/v1/courier");
+    @DisplayName("Test creating courier")
+    @Description("Test creating courier with valid data")
+    public void testCreateCourier() throws Exception {
+        String testLogin = "test" + System.currentTimeMillis();
+        CourierModel courier = new CourierModel(testLogin, "1234", null);
+        Response response = CourierApi.createCourier(courier);
         response.then()
+                .log().all()
                 .statusCode(201)
                 .body("ok", equalTo(true));
-
-        // Login to get courier ID for cleanup
-        Response loginResponse = given()
-                .header("Content-Type", "application/json")
-                .body("{\"login\": \"" + login + "\", \"password\": \"1234\"}")
-                .post("/api/v1/courier/login");
-        courierId = loginResponse.jsonPath().getString("id");
     }
 
     @Test
-    @DisplayName("Cannot create duplicate courier")
-    @Description("Test that creating a courier with an existing login fails")
-    public void testCannotCreateDuplicateCourier() {
-        String login = "ninja" + System.currentTimeMillis();
-        String body = "{\"login\": \"" + login + "\", \"password\": \"1234\", \"firstName\": \"saske\"}";
-        given()
-                .header("Content-Type", "application/json")
-                .body(body)
-                .post("/api/v1/courier")
-                .then()
-                .statusCode(201);
-
-        given()
-                .header("Content-Type", "application/json")
-                .body(body)
-                .post("/api/v1/courier")
-                .then()
-                .statusCode(409)
-                .body("message", equalTo("Этот логин уже используется. Попробуйте другой."));
-
-        // Login to get courier ID for cleanup
-        Response loginResponse = given()
-                .header("Content-Type", "application/json")
-                .body("{\"login\": \"" + login + "\", \"password\": \"1234\"}")
-                .post("/api/v1/courier/login");
-        courierId = loginResponse.jsonPath().getString("id");
-    }
-
-    @Test
-    @DisplayName("Create courier missing field")
-    @Description("Test that creating a courier without a required field fails")
-    public void testCreateCourierMissingField() {
-        String body = "{\"login\": \"ninja" + System.currentTimeMillis() + "\", \"firstName\": \"saske\"}";
-        given()
-                .header("Content-Type", "application/json")
-                .body(body)
-                .post("/api/v1/courier")
-                .then()
+    @DisplayName("Test creating courier without login")
+    @Description("Test that creating courier without login fails")
+    public void testCreateCourierWithoutLogin() throws Exception {
+        CourierModel courier = new CourierModel("", "1234", null); // Без логина и firstName
+        Response response = CourierApi.createCourier(courier);
+        response.then()
+                .log().all()
                 .statusCode(400)
                 .body("message", equalTo("Недостаточно данных для создания учетной записи"));
     }
--- src/test/java/org/example/models/CourierModel.java
+++ src/test/java/org/example/models/CourierModel.java
@@ -2,78 +2,29 @@ package org.example.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class OrderModel {
+public class CourierModel {
+    @JsonProperty("login")
+    private String login;
+
+    @JsonProperty("password")
+    private String password;
+
     @JsonProperty("firstName")
-    private String firstName;
+    private String firstName = null;
 
-    @JsonProperty("lastName")
-    private String lastName;
-
-    @JsonProperty("address")
-    private String address;
-
-    @JsonProperty("metroStation")
-    private int metroStation;
-
-    @JsonProperty("deliveryDate")
-    private String deliveryDate;
-
-    @JsonProperty("deliveryTime")
-    private String deliveryTime;
-
-    @JsonProperty("color")
-    private String color;
-
-    @JsonProperty("rentTime")
-    private int rentTime;
-
-    @JsonProperty("comment")
-    private String comment;
-
-    @JsonProperty("cost")
-    private int cost;
-
-    @JsonProperty("courierId")
-    private String courierId;
-
-    public OrderModel() {
+    public CourierModel() {
     }
 
-    public OrderModel(String firstName, String lastName, String address, int metroStation, String deliveryDate,
-                      String deliveryTime, String color, int rentTime, String comment, int cost) {
+    public CourierModel(String login, String password, String firstName) {
+        this.login = login;
+        this.password = password;
         this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.metroStation = metroStation;
-        this.deliveryDate = deliveryDate;
-        this.deliveryTime = deliveryTime;
-        this.color = color;
-        this.rentTime = rentTime;
-        this.comment = comment;
-        this.cost = cost;
     }
 
-    // Геттеры и сеттеры
+    public String getLogin() { return login; }
+    public void setLogin(String login) { this.login = login; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
     public String getFirstName() { return firstName; }
     public void setFirstName(String firstName) { this.firstName = firstName; }
-    public String getLastName() { return lastName; }
-    public void setLastName(String lastName) { this.lastName = lastName; }
-    public String getAddress() { return address; }
-    public void setAddress(String address) { this.address = address; }
-    public int getMetroStation() { return metroStation; }
-    public void setMetroStation(int metroStation) { this.metroStation = metroStation; }
-    public String getDeliveryDate() { return deliveryDate; }
-    public void setDeliveryDate(String deliveryDate) { this.deliveryDate = deliveryDate; }
-    public String getDeliveryTime() { return deliveryTime; }
-    public void setDeliveryTime(String deliveryTime) { this.deliveryTime = deliveryTime; }
-    public String getColor() { return color; }
-    public void setColor(String color) { this.color = color; }
-    public int getRentTime() { return rentTime; }
-    public void setRentTime(int rentTime) { this.rentTime = rentTime; }
-    public String getComment() { return comment; }
-    public void setComment(String comment) { this.comment = comment; }
-    public int getCost() { return cost; }
-    public void setCost(int cost) { this.cost = cost; }
-    public String getCourierId() { return courierId; }
-    public void setCourierId(String courierId) { this.courierId = courierId; }
 }
